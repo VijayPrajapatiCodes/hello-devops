54cae41f3e76cb80f21f4e1ea499bf3780c7f8f8
Vijay Prajapati <vijayprajapati24645@gmail.com>
2026-09-04 13:57:43 +0530

    Update greeting message in Hello controller

--- src/main/java/com/vijay/Jenkins/Controller/Hello.java
+++ src/main/java/com/vijay/Jenkins/Controller/Hello.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Hello {
     @GetMapping("/hello")
     public String sayHello() {
-      return  "Kya haal chal hai janeman ";
+      return  "i love you umma 😘😘😘";
     }
 }
 
