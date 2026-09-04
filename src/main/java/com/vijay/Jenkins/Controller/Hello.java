0c8c21b8ff5ef44f39ab5f5aef2ed9369dafd97a
Vijay Prajapati <vijayprajapati24645@gmail.com>
2026-09-04 13:46:30 +0530

    Update Hello.java

--- src/main/java/com/vijay/Jenkins/Controller/Hello.java
+++ src/main/java/com/vijay/Jenkins/Controller/Hello.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Hello {
     @GetMapping("/hello")
     public String sayHello() {
-      return  "Hello Jenkins 🚀 Version 3 Developed By Vijay Prajapati";
+      return  "Kya haal chal hai janeman ";
     }
 }
 
