ca35b34cc4d3605f09041b2e367ef7ef68ffa646
Vijay Prajapati <vijayprajapati24645@gmail.com>
2026-09-04 14:01:25 +0530

    Update Hello.java

--- src/main/java/com/vijay/Jenkins/Controller/Hello.java
+++ src/main/java/com/vijay/Jenkins/Controller/Hello.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Hello {
     @GetMapping("/hello")
     public String sayHello() {
-      return  "i love you umma 😘😘😘";
+      return  "Hello Jenkins";
     }
 }
 
